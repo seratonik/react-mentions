873b19e15eafe130fb7bf27fab42cf343ad280d5
Brent Luehr <brent@nerdcon.com>
2016-12-01 14:14:57 -0800

    Re-focus to top suggestion on text changes

--- src/MentionsInput.js
+++ src/MentionsInput.js
@@ -222,8 +222,8 @@ const MentionsInput = React.createClass({
   // Returns the text to set as the value of the textarea with all markups removed
   getPlainText: function() {
     return utils.getPlainText(
-      this.props.value || "", 
-      this.props.markup, 
+      this.props.value || "",
+      this.props.markup,
       this.props.displayTransform
     );
   },
@@ -281,6 +281,7 @@ const MentionsInput = React.createClass({
       selectionStart: selectionStart,
       selectionEnd: selectionEnd,
       setSelectionAfterMentionChange: setSelectionAfterMentionChange,
+      focusIndex: 0
     });
 
     var mentions = utils.getMentions(newValue, this.props.markup);
